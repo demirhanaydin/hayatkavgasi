41fbb2416f115e96905043424e298e0a2a1499ca
Demirhan Aydin <demirhanaydin@gmail.com>
2012-06-01 01:55:24 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hayatkavgasi::Application.config.secret_token = 'd830d541f39dbe2af05785fad66bfa8a34ad2a30adce88ecccc03f37398870905c8fd91e48eab9e1e116e0990f643d54a5e925329e99af416a3b91e3e840f686'
